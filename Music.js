9a9d74e21b8cb2455b58f38b2fafdea5706f838d
iamvengeance <joelemanual0077@gmail.com>
2025-04-14 00:20:48 +0530

    modified

--- Music.js
+++ Music.js
@@ -2,3 +2,14 @@ let x=document.querySelector(".play-music");
 function playaudio(){
     x.play()
 }
+
+
+function settingsnav(){
+    document.querySelector(".settingsoption").style='z-index:101;';
+    document.querySelector(".closebutton").style='z-index:101;';
+}
+
+function closesettingsnav(){
+    document.querySelector(".settingsoption").style='z-index:0;';
+    document.querySelector(".closebutton").style='z-index:0;';
+}
